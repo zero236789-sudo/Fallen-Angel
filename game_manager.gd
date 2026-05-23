64726724a1a321e3e891e9582de76179e4544b3a
Gerysnz <gerardfuentesaf@gmail.com>
2026-05-23 20:14:52 +0200

    enemigos mejorados + temproizador d puntos

--- game_manager.gd
+++ game_manager.gd
@@ -8,17 +8,17 @@ var highscore: int = 0
 var lives: int = 3
 
 func add_score(points: int) -> void:
-    score += points
-    if score > highscore:
-        highscore = score
-    score_changed.emit(score)
+	score += points
+	if score > highscore:
+		highscore = score
+	score_changed.emit(score)
 
 func reset() -> void:
-    score = 0
-    lives = 3
-    score_changed.emit(score)
-    lives_changed.emit(lives)
+	score = 0
+	lives = 3
+	score_changed.emit(score)
+	lives_changed.emit(lives)
 
 func take_damage(amount: int) -> void:
-    lives -= amount
-    lives_changed.emit(lives)
+	lives -= amount
+	lives_changed.emit(lives)
